605f314f71ed11018c0c86a08107728bb460a19c
sl-say <hiexam@163.com>
2019-09-14 12:24:11 +0800

    导入百度语音识别

--- src/modules/system/controllers/ctrl_baidu.js
+++ src/modules/system/controllers/ctrl_baidu.js
@@ -3,7 +3,6 @@ const baidu          = require("../../../core/baidu");
 exports.recognize = async (voiceBuffer) => {
   return new Promise((resolve, reject) => {
     const baduClient = baidu.createBaiduClient();
-    // const option = { dev_pid: '1536' };
     if (baduClient) {
       baduClient.recognize(voiceBuffer, 'pcm', 16000)
         .then((result) => {
